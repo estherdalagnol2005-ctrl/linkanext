af63c31296c30baaa2d5e89506c09372cf84260a
Esther Dal Agnol <estherdalagnol2005@gmail.com>
2026-07-10 19:55:14 -0300

    Refine dark global background

--- components/linka/GlobalBackground.tsx
+++ components/linka/GlobalBackground.tsx
@@ -3,9 +3,7 @@ export default function GlobalBackground() {
     <div className="linka-global-background-v3" aria-hidden="true">
       <span className="lgb3-shape lgb3-blue-panel" />
       <span className="lgb3-shape lgb3-lime-pill" />
-      <span className="lgb3-shape lgb3-cyan-frame" />
       <span className="lgb3-shape lgb3-violet-arc" />
-      <span className="lgb3-shape lgb3-orange-bar" />
       <span className="lgb3-shape lgb3-blue-panel-secondary" />
     </div>
   );
